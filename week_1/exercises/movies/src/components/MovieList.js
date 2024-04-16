ac0854af5162eedfb9ca0c825773ecf0f7a3bc53
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-04-16 11:54:05 +0200

    wip

---
+++ week_1/exercises/movies/src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MovieList({movies}) {
+  return (
+    <div className="list">
+      {movies.map(()=>{
+        return 
+      })}
+    </div>
+  )
+}
