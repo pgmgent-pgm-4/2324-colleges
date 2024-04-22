a8e4b5d7165063bd02bc9b54317aa5b7f0e81da3
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-04-22 14:11:17 +0200

    demo

---
+++ solutions/tryouts/src/components/Buttons.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Buttons() {
+	const DefaultButton = styled.button`
+		background-color: tomato;
+		border: 1px solid gold;
+	`;
+	return <DefaultButton>Buttons</DefaultButton>;
+}
