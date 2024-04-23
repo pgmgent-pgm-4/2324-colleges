9d80866ac814354112f53d670582c68a93df9d4b
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-04-23 11:43:12 +0200

    part beers

--- week_2/demo_01/beer-app/src/components/Beer.js
+++ week_2/demo_01/beer-app/src/components/Beer.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./beer.css";
 
+const ArticleDefault = styled.article`
+	border: 2px solid gold;
+`;
+const ArticleItaly = styled.article`
+	background-color: red;
+`;
 export default function Beer({ beer }) {
+	const [isItaly, setIsItaly] = useState(true);
 	return (
-		<article>
-			<h3>
-				{beer.title} - {beer.alchool}
-			</h3>
-			<p>{beer.description}</p>
-		</article>
+		<>
+			{isItaly ? (
+				<ArticleItaly className={isItaly ? `green` : `red`}>
+					<h3>
+						{beer.title} - {beer.alchool}
+					</h3>
+					<p>{beer.description}</p>
+				</ArticleItaly>
+			) : (
+				<ArticleDefault className={isItaly ? `green` : `red`}>
+					<h3>
+						{beer.title} - {beer.alchool}
+					</h3>
+					<p>{beer.description}</p>
+				</ArticleDefault>
+			)}
+		</>
 	);
 }
