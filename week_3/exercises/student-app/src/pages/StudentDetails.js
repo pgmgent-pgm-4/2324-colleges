51477588686f9cbd29dc38110256893110ddd1d1
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-04-30 15:12:03 +0200

    wip student

--- week_3/exercises/student-app/src/pages/StudentDetails.js
+++ week_3/exercises/student-app/src/pages/StudentDetails.js
@@ -1,8 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import allStudents from "../data/student";
+import Student from "../components/students/Student";
 
 export default function StudentDetails() {
 	const { studentId } = useParams();
-
-	return <div>StudentDetails: {studentId}</div>;
+	const [student, setStudent] = useState(null);
+	useEffect(() => {
+		const newStudent = allStudents.filter(
+			(student) => student.id === studentId
+		);
+		setStudent(newStudent[0]);
+	}, []);
+	return (
+		<div>
+			StudentDetails: {studentId}
+			{student && <Student student={student} />}
+		</div>
+	);
 }
